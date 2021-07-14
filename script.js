50efa2fe7be5096b45e653097df5b33b1d006ea8
Akhtaruzzaman <akhtr01947@gmail.com>
2021-07-14 20:33:32 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var modalparent = document.getElementsByClassName("modal_multi");
+var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
+
+var span_close_multi = document.getElementsByClassName("close_multi");
+
+function setDataIndex() {
+    for (i = 0; i < modal_btn_multi.length; i++) {
+        modal_btn_multi[i].setAttribute('data-index', i);
+        modalparent[i].setAttribute('data-index', i);
+        span_close_multi[i].setAttribute('data-index', i);
+    }
+}
+for (i = 0; i < modal_btn_multi.length; i++) {
+    modal_btn_multi[i].onclick = function () {
+        var ElementIndex = this.getAttribute('data-index');
+        modalparent[ElementIndex].style.display = "block";
+    };
+
+    span_close_multi[i].onclick = function () {
+        var ElementIndex = this.getAttribute('data-index');
+        modalparent[ElementIndex].style.display = "none";
+    };
+}
+window.onload = function () {
+    setDataIndex();
+};
+window.onclick = function (event) {
+    if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+        modalparent[event.target.getAttribute('data-index')].style.display = "none";
+    }
+};
